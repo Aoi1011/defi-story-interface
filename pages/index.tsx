258d2f2a0a17a2dcd319e730b2b4580da869a98b
Aoi <ht.so.ik0102@gmail.com>
2022-08-09 14:07:23 +0700

    Update home

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,7 +1,11 @@
+import React from "react";
+import Link from "next/link";
+
 import type { NextPage } from "next";
 import Head from "next/head";
 import Image from "next/image";
 import Header from "../components/Header";
+import { Navbar } from "../components/Navbar";
 import styles from "../styles/Home.module.css";
 
 const Home: NextPage = () => {
@@ -9,7 +13,21 @@ const Home: NextPage = () => {
     <div>
       <Header />
       <div>
-        <h1 className="text-3xl font-bold underline">DeFi Story</h1>
+        <Navbar>
+          <li className="mr-6">
+            <Link href="/">
+              <a>Home</a>
+            </Link>
+          </li>
+          <li className="mr-6">
+            <Link href="#">
+              <a>About</a>
+            </Link>
+          </li>
+          <li className="mr-6">
+            <a href="#">GitHub</a>
+          </li>
+        </Navbar>
       </div>
     </div>
   );
